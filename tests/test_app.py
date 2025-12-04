924645cd6391c77f1b5c4633ad123be473c0b0c9
WildShrub <erichampson825@gmail.com>
2025-12-04 17:38:43 +0000

    fdfsfhskdfnskdf

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from app import add
+import sys 
+from pathlib import pathlib
+root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root / "src"))
+def test_add():
+    assert add(5+6) = 11
+
+def test_add2():
+    assert add(30+15) = 45
+def test_add3():
+    assert add(5+6) != 10
