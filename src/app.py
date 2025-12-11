5004de2e2bde6cb2baa727fce5b47fb7bfb183c4
WildShrub <erichampson825@gmail.com>
2025-12-11 21:23:20 +0000

    Add mathematical operations: subtract, multiply, divide, log, square, sin, cos, square_root, and percentage functions

--- src/app.py
+++ src/app.py
@@ -1,2 +1,32 @@
+import MATH
+
 def add (a,b):
     return a+b
+
+def subtract (a,b):
+    return a-b
+
+def multiply (a,b):
+    return a*b
+
+def divide (a,b):
+    return a/b
+
+def log (a,b):
+    return MATH.log(a,b)
+
+def square (a):
+    return a^2
+
+def sin (a):
+    return MATH.sin(a)
+
+def cos (a):
+    return MATH.cos(a)
+
+def square_root (a):
+    return MATH.square_root(a)
+
+def percentage (a,b):
+    return (a / b) * 100
+    
